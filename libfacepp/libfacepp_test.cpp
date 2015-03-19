0a475ae32d9218ae01e0e88d9ee347732ec0a01e
JinweiClarkChao <clarkzjw@gmail.com>
2015-03-19 18:33:24 +0800

    init

---
+++ libfacepp/libfacepp_test.cpp
@@ -0,0 +1,15 @@
+#include "libfacepp_core.h"
+
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cout << "Hello Facepp" << endl;
+	return 0;
+}
